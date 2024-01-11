89f9e2be316f7002cabffba9ae08316fa00dbc18
ashwabu <abdulazizredi90@gmail.com>
2024-01-11 16:45:13 +0300

    Define the admin interface

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django admin customization.
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -11,6 +12,22 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin interface"""
     ordering = ['id']
     list_display = ['email', 'name']
+    fields = (
+        (None, {'fields': ('email', 'name')}),
+       (
+            _('Permissions'),
+        {
+            'fields':(
+                'is_staff',
+                'is_active',
+                'is_superuser',
+            )
+        }
+        ),
+        (_('Important dates'), {'fields':('last_login', )})
+        )
+    readonly_fields = ['last_login']
+
 
 
 admin.site.register(models.User, UserAdmin)
