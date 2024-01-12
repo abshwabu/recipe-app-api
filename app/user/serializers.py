402782555ad45aff8ffa1c67b122ec979b6af30e
ashwabu <abdulazizredi90@gmail.com>
2024-01-12 10:03:11 +0300

    Serializer for the User object

---
+++ app/user/serializers.py
@@ -0,0 +1,17 @@
+"""
+Serializer for the User Api view.
+"""
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the User object."""
+
+    class Meta:
+        model = get_user_model()
+        fields = ['email', 'password', 'name']
+        extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
+
+        
