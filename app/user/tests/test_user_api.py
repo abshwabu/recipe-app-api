df95044e85de3b443f3e632e3f8a5d47a296bad5
ashwabu <abdulazizredi90@gmail.com>
2024-01-12 09:53:26 +0300

    Test an error returned if password shorter than 5 characters.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -49,4 +49,17 @@ class PublicUserApiTest(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_password_too_short_error(self):
+        """Test an error returned if password shorter than 5 characters."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'pw',
+            'name':'Test name',
+        }
+        res = self.client.post(CREATE_USER_URL, payload)
 
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        user_exist = get_user_model().objects.filter(
+            email=payload['email']
+        ).exists()
+        self.assertFalse(user_exist)
