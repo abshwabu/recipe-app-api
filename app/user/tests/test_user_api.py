7925dfb7510a4ab2aa66dc7c6e74767c6ba242a6
ashwabu <abdulazizredi90@gmail.com>
2024-01-12 09:44:27 +0300

    Test error returned if user with email exists.

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -37,4 +37,16 @@ class PublicUserApiTest(TestCase):
         self.assertTrue(user.check_password(payload['password']))
         self.assertNotIn('password',res.data)
 
-    
+    def test_user_with_email_exists_error(self):
+        """Test error returned if user with email exists."""
+        payload = {
+            'email': 'test@example.com',
+            'password': 'testPass123',
+            'name':'Test name',
+        }
+        create_user(**payload)
+        res = self.client.post(CREATE_USER_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+
