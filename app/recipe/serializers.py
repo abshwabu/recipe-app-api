9187b20db58bca000abca90183fc6fd042c7f40d
ashwabu <abdulazizredi90@gmail.com>
2024-01-14 17:38:52 +0300

    linting

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -5,11 +5,11 @@ from rest_framework import serializers
 
 from core.models import Recipe
 
+
 class RecipeSerializer(serializers.ModelSerializer):
     """serializer for recipes."""
 
     class Meta:
         model = Recipe
-        fields = ['id','title','time_minutes','price','description','link']
+        fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
-        
