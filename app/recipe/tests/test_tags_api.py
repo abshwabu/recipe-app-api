8817ab5831e7099e360bd22a585312c39b5ff54b
ashwabu <abdulazizredi90@gmail.com>
2024-01-15 14:55:17 +0300

    Test list of tags is limited to authenticated users.

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -52,3 +52,16 @@ class PrivateTagsTests(TestCase):
         serializer = TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
+
+    def test_tags_limited_to_user(self):
+        """Test list of tags is limited to authenticated users."""
+        user2 = create_user(email='user2@example.com')
+        Tag.objects.create(user=user2, name='Fruit')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
+
+        res = self.client.get(TAG_URL)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(res.data), 1)
+        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['id'], tag.id)
