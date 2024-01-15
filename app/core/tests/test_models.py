c5cf66495cff229052da1d05cb2bbab564fbc9ee
ashwabu <abdulazizredi90@gmail.com>
2024-01-15 11:43:26 +0300

    Create and return a new user

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,11 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+def create_user(**params):
+    """Create and return a new user."""
+    return get_user_model().objects.create_user(**params)
+
+
 class ModelTest(TestCase):
     """Test Model."""
 
