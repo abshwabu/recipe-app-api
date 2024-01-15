4b7b196b6d2551b684795fb5a4eb67b105258195
ashwabu <abdulazizredi90@gmail.com>
2024-01-15 14:20:33 +0300

    Create a new user

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,3 +1,20 @@
 """
 Tests for the tags API.
 """
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.urls import reverse
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Tag
+
+from recipe.serializers import TagSerializer
+
+TAG_URL = reverse('recipe:tag-list')
+
+
+def create_user(**params):
+    """Create a new user."""
+    return get_user_model().objects.create_user(**params)
