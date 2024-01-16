f5abdf6bcea507eb02a7639262d900acd27d7f70
ashwabu <abdulazizredi90@gmail.com>
2024-01-16 16:47:28 +0300

    Create and return a ingredient detail URL

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -15,6 +15,11 @@ from recipe.serializers import IngredientSerializer
 INGREDIENT_URL = reverse('recipe:ingredient-list')
 
 
+def detail_url(ingredient_id):
+    """Create and return a ingredient detail URL."""
+    return reverse('recipe:ingredient-detail', args=(ingredient_id,))
+
+
 def create_user(email='user@example.com', password='test123'):
     """Create and return a new user."""
     return get_user_model().objects.create_user(email, password)
