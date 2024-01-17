a9b4883398f2ea1ff0ebf1bfa49dc32565282d23
ashwabu <abdulazizredi90@gmail.com>
2024-01-17 04:26:22 +0300

    Handle creating or getting ingredients.

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -47,6 +47,16 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def _get_or_create_ingredients(self, ingredients, recipe):
+        """Handle creating or getting ingredients."""
+        auth_user = self.context['request'].user
+        for ingredient in ingredients:
+            ingredient_obj, created = Ingredient.objects.get_or_create(
+                user=auth_user,
+                **ingredient,
+            )
+            recipe.ingredients.add(ingredient_obj)
+
     def create(self, validated_data):
         """Create a Recipe."""
         tags = validated_data.pop('tags', [])
