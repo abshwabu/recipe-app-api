f09e39d81d855a7f0ade3f5081a68a4f6c9a5aaa
ashwabu <abdulazizredi90@gmail.com>
2024-01-17 04:31:12 +0300

    error

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -60,7 +60,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a Recipe."""
         tags = validated_data.pop('tags', [])
-        ingredients = validated_data.pop('tags', [])
+        ingredients = validated_data.pop('ingredients', [])
         recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(tags, recipe)
         self._get_or_create_ingredients(ingredients, recipe)
