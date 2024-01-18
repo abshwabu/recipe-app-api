4045b51232d1a3f4b0ed21ce2e283a71e0dc0922
ashwabu <abdulazizredi90@gmail.com>
2024-01-18 10:29:57 +0300

    upload an image to recipe action

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -35,6 +35,18 @@ class RecipeViewSet(viewsets.ModelViewSet):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
 
+    @action(methods=['POST'], detail=True, url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        """upload an image to recipe"""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class BaseRecipeAttrsViewSet(
                  mixins.UpdateModelMixin,
