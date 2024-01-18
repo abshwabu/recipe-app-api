0f790565e9a9e7d3d0a7c4bd290393776088810a
ashwabu <abdulazizredi90@gmail.com>
2024-01-18 10:37:25 +0300

    upload image final

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -393,7 +393,7 @@ class ImageUploadTest(TestCase):
         """Test uploading an image to a recipe."""
         url = image_upload_url(self.recipe.id)
         with tempfile.NamedTemporaryFile(suffix='.jpg') as image_file:
-            img = Image.new('RGB', (10,10))
+            img = Image.new('RGB', (10, 10))
             img.save(image_file, format='JPEG')
             image_file.seek(0)
             payload = {'image': image_file}
@@ -409,6 +409,6 @@ class ImageUploadTest(TestCase):
         """Test uploading invalid image."""
         url = image_upload_url(self.recipe.id)
         payload = {'image': 'Not an image'}
-        res = self.client.post(url, payload, format='multipart/form-data')
+        res = self.client.post(url, payload, format='multipart')
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
