3029261958d1766ec1f9ed19cc6887b5c8cac6f6
ashwabu <abdulazizredi90@gmail.com>
2024-01-20 04:32:22 +0300

    Test the Health Check API.

--- app/core/tests/test_health_check.py
+++ app/core/tests/test_health_check.py
@@ -10,4 +10,11 @@ from rest_framework.test import APIClient
 
 class HealthCheckTest(TestCase):
     """Test the Health Check API."""
-    
+
+    def test_health_check(self):
+        """Test the Health Check API."""
+        clint = APIClient()
+        url = reverse('health-check')
+        res = clint.get(url)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
